b2c6eb4c5d5605ce124831a72e6c5c5408fdff10
Karen Richardson <karenrichardson@Karens-MacBook-Air.local>
2022-07-24 22:48:47 -0400

    added bootstrap and other pages

--- script.js
+++
@@ -1,7 +0,0 @@
-// button.onclick = myFunction;
-
-// function myFunction (){
-
-//     document.getElementsByTagName("BODY")[0].getElementsByClassName.backgroundColor = "pink";
-// console.log("this works");
-// };
